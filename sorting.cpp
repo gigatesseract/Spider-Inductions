d669139073c6566cea5374b53a88e397e299b609
gigatesseract <neevarp.star@gmail.com>
2018-05-07 15:23:23 +0530

    SOrting

---
+++ sorting.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i,j,n,temp, min;
+	cout<<"Enter number of elements: ";
+	cin>>n;
+	int a[n];
+	cout<<"Enter array elements, one by one";
+	for(i=0;i<n;i++) cin>>a[i];
+	for(i=0;i<n;i++)
+	{
+		min = a[i];
+		for(j=i+1;j<n;j++)
+		{
+			if(a[j]<min)
+				min = a[j];
+			pos = j;
+		}
+		temp = a[i];
+		a[i] = a[pos];
+		a[pos] = temp;
+	}
+	cout<<"The array in sorted order is:- "<<endl;
+	for(i=0;i<n;i++)
+		cout<<a[i]<<" ";
+	cout<<endl;
+}
